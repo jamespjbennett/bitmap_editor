6d8e36b9fab45f3c8b28f01bf0953f9a2504b324
jamespjbennett <jamespjbennett@hotmail.com>
2020-01-31 21:28:22 +0000

    failing test for new bitmap drawing regex match

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -1,7 +1,11 @@
 RSpec.describe "BitmapEditor" do
 
   describe 'regex matches' do
-    it 'should correctly match against command for new bitmap image'
+    it 'should correctly match against command for new bitmap image' do
+      bitmap_editor = BitmapEditor.new
+      valid_input = "I 5 6"
+      expect(valid_input.match(clear_command)).not_to be_nil
+    end
     it 'should correctly match against command for single dot on bitmap image'
     it 'should correctly match against command for vertical line on bitmap image'
     it 'should correctly match against command for horizontal line on bitmap image'
