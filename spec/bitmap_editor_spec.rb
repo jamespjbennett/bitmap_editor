3e77266ba2acdecad4130b9ca35d99adb35fcb49
jamespjbennett <jamespjbennett@hotmail.com>
2020-01-31 21:22:50 +0000

    create initial expectaions for regex matches on input commans

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -1,5 +1,13 @@
 RSpec.describe "BitmapEditor" do
 
+  describe 'regex matches' do
+    it 'should correctly match against command for new bitmap image'
+    it 'should correctly match against command for single dot on bitmap image'
+    it 'should correctly match against command for vertical line on bitmap image'
+    it 'should correctly match against command for horizontal line on bitmap image'
+    it 'should correctly match against command for showing bitmap image'
+    it 'should correctly match against command for clearing bitmap image'
 
+  end
 
 end
