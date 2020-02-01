64244692ae476583418e592f296a044e490ce79b
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-01 09:13:25 +0000

    first failing test for bitmap image init

--- spec/bitmap_image_spec.rb
+++ spec/bitmap_image_spec.rb
@@ -3,7 +3,10 @@ require_relative '../lib/bitmap_image'
 RSpec.describe "BitmapImage" do
 
   describe 'initialization' do
-    it 'should initialize with a rows attribute'
+    it 'should initialize with a rows attribute' do
+      bitmap_image = BitmapImage.new
+      expect(bitmap_image.rows).not_to raise_error
+    end
     it 'should initialize with a columns attribute'
     it 'should initialize with a grid attribute'
   end
