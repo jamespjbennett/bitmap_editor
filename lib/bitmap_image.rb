40f5dac81d792e1f5174c727e8dc7c13b4fe6ea0
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-01 09:57:12 +0000

    refactor find integer code  into method for reusability

--- lib/bitmap_image.rb
+++ lib/bitmap_image.rb
@@ -7,10 +7,15 @@ class BitmapImage
   end
 
   def generate_grid(command)
-    int_values = command.scan(/\d+/).map(&:to_i)
-    return "Grid must be less than ot equal to 250 * 250" if int_values.max > 250
+    int_values = command_integer_values(command)
+    return puts "Grid must be less than ot equal to 250 * 250" if int_values.max > 250
     @rows = int_values[1]
     @columns = int_values[0]
     @grid = Array.new(@rows) {Array.new(@columns) {'O'}}
+  end
+
+
+  def command_integer_values(command)
+    command.scan(/\d+/).map(&:to_i)
   end
 end
