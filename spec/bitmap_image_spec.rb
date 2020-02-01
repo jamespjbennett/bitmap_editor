bb17621b9c7db83c1809b6c8638048f58d5264cb
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-01 09:38:25 +0000

    further failing tests for changing the value of bitmap image attributes

--- spec/bitmap_image_spec.rb
+++ spec/bitmap_image_spec.rb
@@ -31,8 +31,12 @@ RSpec.describe "BitmapImage" do
       it 'should successfully change the value of grid' do
         expect(@bitmap_image.instance_variable_get(:@grid)).not_to be_nil
       end
-      it 'should successfully change the value of rows'
-      it 'should successfully change the value of columns'
+      it 'should successfully change the value of rows' do
+        expect(@bitmap_image.instance_variable_get(:@rows)).not_to be_nil
+      end
+      it 'should successfully change the value of columns' do
+        expect(@bitmap_image.instance_variable_get(:@columns)).not_to be_nil
+      end
       it 'should create a multidimensional array'
       it 'should create the correct number of rows as specified by the command'
       it 'should create the correct length of individual rows as specified by the command'
