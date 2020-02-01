edac5a260fdfb11d3ad5df03b99f3764750f74d9
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-01 09:11:47 +0000

    create first skeleton tests for bitmpa image

--- spec/bitmap_image_spec.rb
+++ spec/bitmap_image_spec.rb
@@ -2,6 +2,10 @@ require_relative '../lib/bitmap_image'
 
 RSpec.describe "BitmapImage" do
 
-
+  describe 'initialization' do
+    it 'should initialize with a rows attribute'
+    it 'should initialize with a columns attribute'
+    it 'should initialize with a grid attribute'
+  end
 
 end
