d915e208021826cca6d15d23e00d59c4c0e376d7
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-02 21:56:28 +0000

    passing test for valid single bitmap dot color action

--- lib/bitmap_image.rb
+++ lib/bitmap_image.rb
@@ -18,4 +18,15 @@ class BitmapImage
   def command_integer_values(command)
     command.scan(/\d+/).map(&:to_i)
   end
+
+  def command_string_values(command)
+    command.scan(/[a-zA-Z]/)
+  end
+
+  def colour_single_pixel(command)
+    x_axis_coordinate = command_integer_values(command)[0].to_i
+    y_axis_coordinate = command_integer_values(command)[1].to_i
+    colour = command_string_values(command).last
+    @grid[x_axis_coordinate][y_axis_coordinate] = colour
+  end
 end
