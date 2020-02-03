42e44ebdcb6a996a135f039adce915909173a2eb
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-03 15:16:05 +0000

    passing test for out of bounds vertical line validation

--- lib/bitmap_image.rb
+++ lib/bitmap_image.rb
@@ -23,15 +23,17 @@ class BitmapImage
     command.scan(/[a-zA-Z]/)
   end
 
-  def within_bounds?(coordinates)
-    (coordinates[0] <= @rows) && (coordinates[1] <= @columns)
+  def within_bounds?(x_coordinates, y_coordinates)
+    x_coordinates_valid = x_coordinates.map{|coordinate| coordinate + 1 <= @columns}.uniq == [true]
+    y_coordinates_valid = y_coordinates.map{|coordinate| coordinate + 1 <= @rows}.uniq == [true]
+    x_coordinates_valid && y_coordinates_valid
   end
 
   def colour_single_pixel(command)
     int_values = command_integer_values(command)
     x_axis_coordinate = int_values[0] - 1
     y_axis_coordinate = int_values[1] - 1
-    return puts "Coordinates out of bounds" if !within_bounds?([x_axis_coordinate, y_axis_coordinate])
+    return puts "Coordinates out of bounds" if !within_bounds?([x_axis_coordinate], [y_axis_coordinate])
     colour = command_string_values(command).last
     @grid[x_axis_coordinate][y_axis_coordinate] = colour
   end
@@ -41,6 +43,7 @@ class BitmapImage
     x_axis_coordinate = int_values[0].to_i - 1
     y_axis_coordinate_1 = int_values[1].to_i - 1
     y_axis_coordinate_2 = int_values[2].to_i - 1
+    return puts "Coordinates out of bounds" if !within_bounds?([x_axis_coordinate], [y_axis_coordinate_1, y_axis_coordinate_2])
     colour = command_string_values(command).last
     @grid[y_axis_coordinate_1..y_axis_coordinate_2].each{|row| row[x_axis_coordinate] = colour}
   end
