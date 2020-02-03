f1be35be5041b9c36c6f66f99cc180359a704196
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-03 13:40:51 +0000

    refactor file creation

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -45,25 +45,40 @@ RSpec.describe "BitmapEditor" do
 
 
   describe 'running the file reader' do
-    it 'should create a new bitmap image when the correct command is input' do
-      file = Tempfile.new('test_instructions')
-      file.write("i 5 6")
-      file.read
-      @bitmap_editor.run(file)
-      expect(@bitmap_editor.instance_variable_get(:@grid)).not_to be_nil
-      file.close
-      file.unlink
+    context 'valid file content' do
+      it 'should create a new bitmap image when the correct command is input' do
+        file = open_and_write_temp_file("i 5 6")
+        @bitmap_editor.run(file)
+        expect(@bitmap_editor.instance_variable_get(:@grid)).not_to be_nil
+        remove_temp_file(file)
+      end
+
+      it 'should create a new bitmap image when the correct set of commands are input' do
+        file = Tempfile.new('test_instructions')
+        file.write("i 5 6\nL 1 3 A")
+        file.read
+        @bitmap_editor.run(file)
+        grid = @bitmap_editor.instance_variable_get(:@grid)
+        expect(grid[0][2]).to eq("A")
+        file.close
+        file.unlink
+      end
     end
 
-    it 'should create a new bitmap image when the correct set of commands are input' do
-      file = Tempfile.new('test_instructions')
-      file.write("i 5 6\nL 1 3 A")
-      file.read
-      @bitmap_editor.run(file)
-      grid = @bitmap_editor.instance_variable_get(:@grid)
-      expect(grid[0][2]).to eq("A")
-      file.close
-      file.unlink
+    context 'invalid file content' do
+
     end
+  end
+
+  def open_and_write_temp_file(content)
+    file = Tempfile.new('test_instructions')
+    file.write(content)
+    file.read
+    file
+  end
+
+  def remove_temp_file(file)
+    file.close
+    file.unlink
   end
 end
